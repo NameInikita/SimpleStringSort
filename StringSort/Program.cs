d0c78d94fa4ad37bfd3262f654c19963061815ab
NameInikita <name.inikita@gmail.com>
2017-07-17 21:59:38 +0300

    SimpleStrSort

---
+++ StringSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] ch = str.ToCharArray();
+            Array.Sort(ch);
+            foreach (char elem in ch)
+                Console.Write("{0} ", elem);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
